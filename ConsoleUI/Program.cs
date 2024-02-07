bccb174e5de16a5a14fc007f9c044a1112c896fd
Pierre Plourde <pierre@spartancsharp.net>
2024-02-07 10:29:53 -0500

    Create Code
    Created a Console Application that has variables to hold a person's name, age, if they are alive, and their phone number.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,20 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
+			string firstname = "Pierre";
+			string lastname = "Plourde";
+
+			string fullname = $"{firstname} {lastname}";
+
+			int age = 47;
+
+			bool isalive = true;
+
+			string phonenumber = "905-439-7645";
+
+			Console.WriteLine($"Hi, {fullname}, you are {age} years old, your phone number is {phonenumber}, and you are alive? {isalive}.");
+
+			_ = Console.ReadLine();
 		}
 	}
 }
